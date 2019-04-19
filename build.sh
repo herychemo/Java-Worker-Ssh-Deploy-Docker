35817f6f4cc069f8cf89a153bc6323ea092ca8dd
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-04-19 16:11:57 -0500

    small improvements

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ docker build --rm -t herychemo/java-worker-ssh:0.1.0 .
 
 docker tag herychemo/java-worker-ssh:0.1.0 herychemo/java-worker-ssh:latest
 
+IDS=`docker images | grep "\<none\>" | awk '{print $3}'`
+if [[ ! -z "$IDS" ]]; then
+  docker rmi -f $IDS
+fi
--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 
 docker run \
-    -d -p 22:22 \
+    -d -p 8022:22 \
+    -p 5000:5000 \
     --name java-worker \
     herychemo/java-worker-ssh:latest
 
